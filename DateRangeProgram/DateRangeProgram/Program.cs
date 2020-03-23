cd22a47214f8a9f346a994a46434e233cb15c9d8
Patryk Duda <patryczkov@users.noreply.github.com>
2020-03-23 13:58:20 +0100

    Add solution with initial code

---
+++ DateRangeProgram/DateRangeProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DateRangeProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
