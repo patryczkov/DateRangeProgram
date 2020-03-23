1edbe8425a44b6b2b40fb10c3834f7b7086d82eb
Patryk Duda <patryczkov@users.noreply.github.com>
2020-03-23 18:07:34 +0100

    Create IDateValidation.cs

---
+++ DateRangeProgram/DateRangeProgram/Models/IDateValidation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DateRangeProgram.Models
+{
+    interface IDateValidation
+    {
+        string DetectCulture();
+        bool ValidateDate(string culture);
+    }
+}
