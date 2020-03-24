c41066cdb15fcb82f376ab6793ac163bc0dc2736
Patryk Duda <patryczkov@users.noreply.github.com>
2020-03-24 11:41:14 +0100

    Create IDateParser.cs

---
+++ DateRangeProgram/DateRangeProgram/Models/IDateParser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DateRangeProgram.Models
+{
+    public interface IDateParser
+    {
+        public Date ParseStringIntoDate(string dateString);
+    }
+}
