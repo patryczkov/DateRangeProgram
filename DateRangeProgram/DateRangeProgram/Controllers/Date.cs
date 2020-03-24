518fbfcd96249825b32db3d2f450f620ecfb04ba
Patryk Duda <patryczkov@users.noreply.github.com>
2020-03-24 17:22:49 +0100

    Fix of missing Date class

---
+++ DateRangeProgram/DateRangeProgram/Controllers/Date.cs
@@ -0,0 +1,15 @@
+﻿namespace DateRangeProgram.Models
+{
+    public class Date
+    {
+        public int Day { get; }
+        public int Month { get; }
+        public int Year { get; }
+        public Date(int day, int month, int year)
+        {
+            Day = day;
+            Month = month;
+            Year = year;
+        }
+    }
+}
