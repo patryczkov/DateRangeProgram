52464d2862601b8c9601e389ac18898e5f37e1ca
Patryk Duda <patryczkov@users.noreply.github.com>
2020-03-24 12:33:31 +0100

    Add method to check if dates are proper

--- DateRangeProgram/DateRangeProgram/Controllers/DateController.cs
+++ DateRangeProgram/DateRangeProgram/Controllers/DateController.cs
@@ -1,9 +1,31 @@
-﻿namespace DateRangeProgram
+﻿using DateRangeProgram.Models;
+
+namespace DateRangeProgram
 {
     public class DateController
     {
-        public DateController()
+        private const int FIRST_DATE_NUMERIC_REPRESENTATION = 1;
+        private const int SECOND_DATE_NUMERIC_REPRESENTATION = 2;
+        private const int PROPER_DATES_NUMERIC_REPRESENTATION = 0;
+
+        private readonly IDateValidation _dateValidation;
+        private readonly IDateParser _dateParser;
+        private readonly ICalculateDateRange _calculateDateRange;
+
+        public DateController(IDateValidation dateValidation, IDateParser dateParser, ICalculateDateRange calculateDateRange)
         {
+            _dateValidation = dateValidation;
+            _dateParser = dateParser;
+            _calculateDateRange = calculateDateRange;
         }
+        //it points which date is incorrect so it can send proper message to user
+        public int CheckIfDatesAreProper(string firstDateString, string secondDateString)
+        {
+            if (!_dateValidation.ValidateDate(firstDateString)) return FIRST_DATE_NUMERIC_REPRESENTATION;
+            if (!_dateValidation.ValidateDate(secondDateString)) return SECOND_DATE_NUMERIC_REPRESENTATION;
+            return PROPER_DATES_NUMERIC_REPRESENTATION;
+        }
+
+
     }
 }
