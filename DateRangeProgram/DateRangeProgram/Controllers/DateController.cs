a12a15885cf8e7bda48067a271086c36185441c4
Patryk Duda <patryczkov@users.noreply.github.com>
2020-03-24 12:55:37 +0100

    Add 2nd stage of date validation
    -add check got dates diffrence,
    -rename view to dateview,
    -add view methots to prints errors,

--- DateRangeProgram/DateRangeProgram/Controllers/DateController.cs
+++ DateRangeProgram/DateRangeProgram/Controllers/DateController.cs
@@ -9,39 +9,50 @@ namespace DateRangeProgram
         private const int SECOND_DATE_NUMERIC_REPRESENTATION = 2;
         private const int PROPER_DATES_NUMERIC_REPRESENTATION = 0;
 
+        private const int DAY_INDEX = 0;
+        private const int MONTH_INDEX = 1;
+        private const int YEAR_INDEX = 2;
+
         private readonly IDateValidation _dateValidation;
         private readonly IDateParser _dateParser;
         private readonly ICalculateDateRange _calculateDateRange;
-        private readonly ResultView _resultView;
+        private readonly DateView _view;
 
-        public DateController(IDateValidation dateValidation, IDateParser dateParser, ICalculateDateRange calculateDateRange, ResultView resultView)
+        public DateController(IDateValidation dateValidation, IDateParser dateParser, ICalculateDateRange calculateDateRange, DateView view)
         {
             _dateValidation = dateValidation;
             _dateParser = dateParser;
             _calculateDateRange = calculateDateRange;
-            _resultView = resultView;
+            _view = view;
         }
         
         public void GetDatesRange(string firstDateString, string secondDateString)
         {
             var checkStatus = CheckIfDatesAreProper(firstDateString, secondDateString);
-            if (checkStatus != 0)
+            if (checkStatus != PROPER_DATES_NUMERIC_REPRESENTATION)
             {
-                _resultView.PrintWrongDateFormat(checkStatus);
+                _view.WrongDateFormatError(checkStatus);
                 return;
             }
 
             var firstDate = _dateParser.ParseStringIntoDate(firstDateString);
             var secondDate = _dateParser.ParseStringIntoDate(secondDateString);
 
+            if(_calculateDateRange.CheckIfFirstDateIsSmallerThansSecondOne(firstDate, secondDate))
+            {
+                _view.FirstDateIsBiggerError();
+                return;
+            }
 
+            var datesDiffrenceArray = _calculateDateRange.CalulateDiffrenceBetweenDates(firstDate, secondDate);
 
+            if(datesDiffrenceArray[YEAR_INDEX] != 0)
+            {
+                _view.PrintResultWithBothYear(firstDateString, secondDateString)
+            }
 
         }
-        
-        
-        
-        
+          
         //it points which date is incorrect so it can send proper message to user
         private int CheckIfDatesAreProper(string firstDateString, string secondDateString)
         {
@@ -49,10 +60,6 @@ namespace DateRangeProgram
             if (!_dateValidation.ValidateDate(secondDateString)) return SECOND_DATE_NUMERIC_REPRESENTATION;
             return PROPER_DATES_NUMERIC_REPRESENTATION;
         }
-
-
-
-
 
     }
 }
---
+++ DateRangeProgram/DateRangeProgram/Views/DateView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DateRangeProgram.Views
+{
+    public class DateView
+    {
+        public void WrongDateFormatError(int dateNumericRepresentation)
+        {
+            Console.WriteLine($"Date nr= {dateNumericRepresentation} format is invalid");
+        }
+        public void FirstDateIsBiggerError()
+        {
+            Console.WriteLine("First date is bigger then second one");
+        }
+
+        public void PrintResultWithBothYear(string firstDateString, string secondDateString)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
