9a7b7f2b370f9e3a5ab54248240cdd0eca73837f
Patryk Duda <patryczkov@users.noreply.github.com>
2020-03-25 14:28:50 +0100

    Create CalculateDateRangeTest.cs
    -add test if method proper detects smaller 1st date

---
+++ DateRangeProgram/DateRangeProgram.Tests/ModelsTests/CalculateDateRangeTest.cs
@@ -0,0 +1,37 @@
+﻿using DateRangeProgram.Models;
+using NUnit.Framework;
+
+
+namespace DateRangeProgram.Tests.ModelsTests
+{
+    [TestFixture]
+    public class CalculateDateRangeTest
+    {
+        public CalculateDateRange calculateDateRange;
+
+
+        [SetUp]
+        public void SetupClass()
+        {
+            calculateDateRange = new CalculateDateRange();
+        }
+        
+        [Test]
+        public void CheckIfFirstDateIs_Smaller_ThanSecondDate()
+        {
+            //arrange
+            var firstDate = new Date(01,01,2000);
+            var secondDate = new Date(02,01,2000);
+
+            //act
+            var result = calculateDateRange.CheckIfFirstDateIsSmallerThansSecondOne(firstDate, secondDate);
+            
+            //assert
+            Assert.IsTrue(result);
+        }
+   
+
+
+
+    }
+}
